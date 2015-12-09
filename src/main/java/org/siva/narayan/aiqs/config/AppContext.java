64965564bb1918000868f2eef6dcaf42406e112d
Siva Narayana Reddy Maddula <siva_narayanareddy_maddula@apple.com>
2015-12-09 19:25:52 +0530

    Added in-memory super users authentication configuration

--- src/main/java/org/siva/narayan/aiqs/config/AppContext.java
+++ src/main/java/org/siva/narayan/aiqs/config/AppContext.java
@@ -1,10 +1,19 @@
 package org.siva.narayan.aiqs.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @ComponentScan(basePackages = { "org.siva.narayan.aiqs" })
 public class AppContext {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer() {{
+			setLocations(new ClassPathResource("security/super-user.properties"));
+		}};
+	}
 }
--- src/main/java/org/siva/narayan/aiqs/config/web/security/AppSecurityContext.java
+++ src/main/java/org/siva/narayan/aiqs/config/web/security/AppSecurityContext.java
@@ -1,8 +1,12 @@
 package org.siva.narayan.aiqs.config.web.security;
 
+import java.util.Arrays;
+
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,18 +18,28 @@ public class AppSecurityContext extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private DataSource aiqsDataSource;
-	
-	@Override
-	@Autowired
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		auth
-			.jdbcAuthentication()
-			.dataSource(aiqsDataSource)
-			// select username,password,enabled from users where username = ? -- this is the query which spring uses
-		    .usersByUsernameQuery("SELECT USER_ID AS USERNAME, PASSWORD, ENABLED FROM LOGIN_CREDENTIALS WHERE USER_ID = ?")
-			//select username,authority from authorities where username = ? -- this is the query which spring uses
-		    .authoritiesByUsernameQuery("SELECT USER_ID AS USERNAME, AUTHORITY FROM AUTHORITIES WHERE USER_ID = ?");
+
+	@Value(value = "${super_users}")
+	private String superUsers;
+
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("superUsers === " + superUsers);
 	}
-	
+
+	@Autowired
+	protected void configureGlobalUsers(AuthenticationManagerBuilder auth) throws Exception {
+		for (final String userPrincipal : superUsers.split("&")) {
+			final String userDetails[] = userPrincipal.split(":");
+			if(userDetails.length > 0) {
+				auth.inMemoryAuthentication()
+						.withUser(userDetails[0])
+							.password(userDetails[1])
+								.authorities("SUPER_ADMIN");
+			} else {
+				System.err.println("Super User Configuration Error :: "+ Arrays.toString(userDetails));
+			}
+		}
+	}
+
 }
